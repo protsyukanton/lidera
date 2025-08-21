97e15c77d61720e15ee372500095379c8bf6a2bc
Антон <anton@MacBook-Air-Anton.local>
2025-08-21 20:04:36 +0300

    feat: add popup

--- src/js/main.js
+++ src/js/main.js
@@ -23,4 +23,62 @@ document.addEventListener('DOMContentLoaded', function () {
         controls: ['play-large']
     });
 
+
+    /* ---------- Popup ---------- */
+
+    /* Ищем все кнопки открытия popup окон и помещаем их в переменную "popupButtons" */
+    const popupButtons = document.querySelectorAll(".open-popup");
+
+    /* Вещаем на каждую кнопку открытия popup-окон событие клик */
+    popupButtons.forEach(function (button) {
+        button.addEventListener("click", function () {
+            /* Получаем атрибут "data-popup" у кнопки, на которую кликнули */
+            const popupId = this.getAttribute("data-popup");
+            /* Получаем обертку popup-окна, data-popup которого соответствует кнопке, на которую кликнули */
+            const popupWrapper = document.querySelector(
+                `.popup-wrapper[data-popup="${popupId}"]`
+            );
+            /* На всвякий случай проверяем, есть ли соответствующее кнопке popup окно и добавляем активный класс для показа */
+            if (popupWrapper) {
+                popupWrapper.classList.add("popup-wrapper-show");
+            }
+        });
+    });
+
+    /* Получаем все кнопки закрытия popup окон */
+    const popupCloseButtons = document.querySelectorAll(".popup__close");
+
+    /* Вещаем на каждую кнопку закрытия popup-окон событие клик */
+    popupCloseButtons.forEach(function (button) {
+        button.addEventListener("click", function () {
+            /* Получаем все обертки popup-окон и удаляем у низ активный класс */
+            const popupWrappers = document.querySelectorAll(".popup-wrapper");
+            popupWrappers.forEach(function (popupWrapper) {
+                popupWrapper.classList.remove("popup-wrapper-show");
+            });
+        });
+    });
+
+    /* 
+        Закрытие popup-окна кликом вне его
+        Вешаем событие клик на страницу
+        Ищем все popup-окна, обертки которых имеют активный класс
+    */
+    document.addEventListener("click", function (e) {
+        const popupWrappersVisible = document.querySelectorAll(
+            ".popup-wrapper.popup-wrapper-show"
+        );
+
+        /* Проверяем на что мы кликнули, если это popup-окно или его дочерние элементы или кнопка открытия popup-окна, то помещаем в переменные */
+        popupWrappersVisible.forEach(function (popupWrapper) {
+            const isPopupOrChild =
+                e.target.classList.contains("popup") || e.target.closest(".popup");
+            const isPopupButton = e.target.classList.contains("open-popup");
+            /* Мы должны сделать отрицание наших переменных - если это не окно, его дочерние элементы или кнопка открытия окна, то удаляем активный класс */
+            if (!isPopupOrChild && !isPopupButton) {
+                popupWrapper.classList.remove("popup-wrapper-show");
+            }
+        });
+    });
+
 });
